022ea57992e6ce0e748b72cae570954f642fa543
jittel <nathan.c.ginn@gmail.com>
2022-04-08 14:26:51 -0700

    fixed url, fetch works fully now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 var infoList = document.querySelector('#info');
 var fetchButton = document.querySelector('#fetch-button');
-var format = "websites";
+var format = "maps";
 
 function getApi() {
     console.log("click")
     var searchItem = document.querySelector("#search").value;
     console.log(searchItem)
 
-    // TODO: figure out full url
-    var requestUrl = `https://www.loc.gov/${format}/?q=${searchItem}?fo=json&c=10`
-    // var requestUrl = "https://www.loc.gov/collections/civil-war-maps?fo=json"
+    // full url
+    var requestUrl = `https://www.loc.gov/${format}/?q=${searchItem}&fo=json`
 
     fetch(requestUrl)
         .then(function (response) {
@@ -17,21 +16,21 @@ function getApi() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.json())
-            //looping over the fetch response and inserting the results into a list
-            // for (let i = 0; i < data.featured_items.length; i++) {
-            //     const element = data.featured_items[i];
-            //     // console.log(element)
-            //     // create a list element
-            //     var listItem = document.createElement("li");
+            console.log(data)
+            // looping over the fetch response and inserting the results into a list
+            for (let i = 0; i < data.results.length; i++) {
+                const element = data.results[i];
+                console.log(element)
+                // create a list element
+                var listItem = document.createElement("li");
 
-            //     //Set the text of the list element to the JSON response (TODO: need to check for actual responses still)
-            //     // listItem.textContent = JSON.stringify(data.featured_items[i])
-            //     listItem.textContent = data.featured_items[i].title
+                //Set the text of the list element to the JSON response (TODO: need to check for actual responses still)
+                // listItem.textContent = JSON.stringify(data.results[i])
+                listItem.textContent = data.results[i].title
 
-            //     //Append the li element to the id associated with the ul element.
-            //     infoList.appendChild(listItem);
-            // }
+                //Append the li element to the id associated with the ul element.
+                infoList.appendChild(listItem);
+            }
         })
 }
 
